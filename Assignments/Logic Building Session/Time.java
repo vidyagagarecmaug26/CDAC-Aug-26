437ca4e18a8641db768b94cb17fd0f97634b5d09
vidyagagarecmaug26 <vidya.gagare.cmaug26@gmail.com>
2026-08-17 21:36:39 +0530

    Add files via upload

---
+++ Assignments/Logic Building Session/Time.java
@@ -0,0 +1,10 @@
+class Time{
+  public static void main(String[] args){
+         int time=8;
+         if(time>=5 && time<=12){
+             System.out.println("Good Morning");
+         }
+            
+    }
+
+}
