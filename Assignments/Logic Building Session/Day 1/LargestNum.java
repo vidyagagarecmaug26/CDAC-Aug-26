53efc1eee8650af5c5307716082c8bb1a2a87304
vidyagagarecmaug26 <vidya.gagare.cmaug26@gmail.com>
2026-08-18 17:51:48 +0530

    Add files via upload

---
+++ Assignments/Logic Building Session/Day 1/LargestNum.java
@@ -0,0 +1,23 @@
+class LargestNum{
+  public static void main(String[] args){
+         int num1=80;
+         int num2=20;
+         int num3=50;
+
+    
+         if(num1>num2 && num1>num3){
+             System.out.println("largest Number="+num1);
+         }
+         else if(num2>num3 && num2>num1){
+             System.out.println("largest Number="+num2);
+         }
+         else{
+             System.out.println("largest Number="+num3);
+
+         }
+
+         
+            
+    }
+
+}
---
+++ Assignments/Logic Building Session/Day 1/Negative.java
@@ -0,0 +1,14 @@
+class Negative{
+  public static void main(String[] args){
+         int num=-1;
+         if(num<0){
+             System.out.println("Number is Negative");
+         }
+         else{
+             System.out.println("Number is Positive");
+
+         }
+    
+    }
+
+}
